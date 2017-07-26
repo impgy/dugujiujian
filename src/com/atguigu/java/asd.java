cb6cb0250c3265e0d68d5569ddfdcaddffc51101
imp <2601411314@qq.com>
2017-07-26 18:59:28 +0800

    asfa

--- src/com/atguigu/java/asd.java
+++ src/com/atguigu/java/asd.java
@@ -4,7 +4,7 @@ public class asd {
 	public static void main(String[] args) {
 		
 		System.out.println("ss");
-		System.out.println("ss");	
+		System.out.println("sss");	
 		System.out.println("ssd");
 		System.out.println("ss");
 	}
